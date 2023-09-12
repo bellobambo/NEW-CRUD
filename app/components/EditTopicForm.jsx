4f2d1da162ad4dd5d32f5ce41ed578be8d13322d
bellobambo <bellobambo21@gmail.com>
2023-09-12 09:55:17 +0100

    completed

--- app/components/EditTopicForm.jsx
+++ app/components/EditTopicForm.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+"use client"
 
-export default function EditTopicForm() {
+import React, { useState } from 'react'
+import { useRouter } from "next/navigation"
+
+export default function EditTopicForm({ id, title, description }) {
+
+  const [newTitle, setNewTitle] = useState(title)
+  const [newDescription, setNewDescription] = useState(description)
+
+  const router = useRouter()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify({ newTitle, newDescription })
+      })
+      if (!res.ok) {
+        throw new Error("Failed to Update topic")
+      }
+      router.refresh()
+      router.push("/")
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div>
-      <form className='flex flex-col gap-2'>
-        <input className='border border-slate-500 px-8 py-2' type="text" placeholder='Topic Title' />
-        <input className='border border-slate-500 px-8 py-2' type="text" placeholder='Topic Description' />
+      <form onSubmit={handleSubmit} className='flex flex-col gap-2'>
+        <input onChange={(e) => setNewTitle(e.target.value)} value={newTitle} className='border border-slate-500 px-8 py-2' type="text" placeholder='Topic Title' />
+        <input onChange={(e) => setNewDescription(e.target.value)} value={newDescription} className='border border-slate-500 px-8 py-2' type="text" placeholder='Topic Description' />
 
         <button className='bg-green-600 font-bold text-white rounded-md py-2 px-5 w-fit'>Update Topic</button>
       </form>
--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import EditTopicForm from '../../components/EditTopicForm'
-// import EditTopicForm from "@/components/EditTopicForm"
 
-export default function EditTopic() {
+
+const getTopicById = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/topics/${id}`, { cache: 'no-store' })
+
+    if (!res.ok) {
+      throw new Error("failed to fetch topic")
+    }
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default async function EditTopic({ params }) {
+  const { id } = params
+  const { topic } = await getTopicById(id)
+  const {title, description} = topic
+  console.log(id)
+
   return (
-      <div>
-      <EditTopicForm />
+    <div>
+      <EditTopicForm id={id} title={title} description={description} />
     </div>
   )
 }
